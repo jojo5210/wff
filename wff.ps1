80e21cb7c2b2a14a496703f3d9ab10cc0511745b
jojo5210 <111311728+jojo5210@users.noreply.github.com>
2026-05-01 15:41:50 +0330

    wff.ps1

--- wff.ps1
+++ wff.ps1
@@ -87,7 +87,7 @@ Write-Host @"
 |__|\__\ |__|  |__| /__/     \__\ /__/     \__\ |__| \__| 
                                                                                                                                                                                                                         
                                                       
-GamerKhaan Ping Monitor - Online Gaming Perfected!
+RoYa︻デ══━一 Ping Monitor - Online Gaming Perfected!  ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з=(◣_◢)=ε/̵͇̿̿/'̿'̿ ̿̿ 
 "@ -ForegroundColor Cyan
 Write-Host "Quest started at $($startTime.ToString('yyyy-MM-dd HH:mm:ss'))" -ForegroundColor Gray
 Write-Host "============================================================================" -ForegroundColor Cyan
